5bfe40c051269d8f878fedaafb05d2541e10cd19
simonakomarovska <simonak399@gmail.com>
2024-04-29 15:17:05 +0200

    fixed responsive

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ const add = document.querySelector(".add")
 subtract.addEventListener("click", function() {count.innerHTML--})
 add.addEventListener("click", function() {count.innerHTML++})
 reset.addEventListener("click", function() {count.innerHTML = 0})
+
+
+// function subtractOne() {
+//     count.innerHTML--;
+// }
+// function addOne() {
+//     count.innerHTML++;
+// }
+// function resetCount() {
+//     count.innerHTML = 0;
+// }
+// subtract.addEventListener("click", subtractOne);
+// add.addEventListener("click", addOne);
+// reset.addEventListener("click", resetCount);
